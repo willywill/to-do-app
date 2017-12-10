851a81b0e55d0c8bfc4b777c48c91a08075095dd
Will Germany <germany.william@gmail.com>
2017-12-09 22:36:59 -0500

    Added the initial Express back-end

---
+++ back-end/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// Start our express server
+const app = express()
+
+// Define a ENV port or increment the default Vue.js port number (8080)
+const port = process.env.PORT || 8081
+
+/*
+  Just as a reminder, a quick list of common RESTful methods and meanings:
+  GET    - Retrieve a resource
+  POST   - Create a resource
+  PUT    - Create a resource. However, if it exist already, update it
+  PATCH  - Update a partial resource (saves bandwidth over PUT)
+  DELETE - Delete a resource
+*/
+
+// Initialize the middleware
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Create an example route for the home page
+app.get('/', async (req, res) => {
+  res.send("Hello, World!")
+})
+
+app.listen(port, async () => {
+  console.log(`Sever successfully started on port ${port}.`)
+})
