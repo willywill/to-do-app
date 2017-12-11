6046d52db33fbbacc628c097809f3371cfa40f74
Will Germany <germany.william@gmail.com>
2017-12-11 10:39:35 -0500

    Added logout functionality

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -3,7 +3,7 @@
    role="navigation"
    aria-label="main navigation">
   <div class="navbar-end">
-    <router-link class="navbar-item" to="/">
+    <router-link v-if="!$store.state.isUserLoggedIn" class="navbar-item" to="/">
       Home
     </router-link>
     <router-link v-if="!$store.state.isUserLoggedIn" class="navbar-item" to="/login">
@@ -12,6 +12,9 @@
     <router-link v-if="!$store.state.isUserLoggedIn" class="navbar-item" to="/register">
       Sign Up
     </router-link>
+    <a v-if="$store.state.isUserLoggedIn" v-on:click="logout" class="navbar-item">
+      Logout
+    </a>
 
     <button class="button navbar-burger">
       <span></span>
@@ -24,6 +27,15 @@
 
 <script>
 export default {
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'home'
+      })
+    }
+  }
 }
 </script>
 
--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -58,6 +58,9 @@ export default {
 
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({
+          name: 'home'
+        })
       } catch (error) {
         this.error = error.response.data.error
       }
